3124f0d214e0492c9479d4ce4c866954086aa7bc
BollareddyPranoyRaj1 <bollareddypranoyraj@gmail.com>
2026-05-28 10:27:34 +0530

    Add list operations and print statements to 1stClass.py

--- 1stClass.py
+++ 1stClass.py
@@ -1,1 +1,19 @@
 print("hello")
+
+#list
+list1 = [1,2,"python","programming",3.14]
+print(list1)
+
+list1.append("hello")
+print(list1)
+
+list1.insert(2,"world")
+print(list1)
+
+list1.remove(3.14)
+print(list1)
+
+list1.pop(3)
+print(list1)
+
+print(len(list1))
